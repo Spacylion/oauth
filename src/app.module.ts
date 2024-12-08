f4d6dec155544ed3ec9ed07cb1cb0b4c62db2c02
spacylion <fuerstein@yandex.ru>
2024-12-09 00:44:42 +0300

    feat: env separation, logger configuration.ts set up, userService, prettier, eslinter set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { configuration } from '@/config/index'
@@ -36,21 +35,3 @@ export class AppModule {
 		}
 	}
 }
-=======
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'
-import { IS_DEV_ENV } from '@/libs/common/is-dev-env'
-
-@Module({
-  imports: [
-      ConfigModule.forRoot({
-
-        isGlobal: true,
-        ignoreEnvFile: !IS_DEV_ENV
-      })
-  ],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
->>>>>>> 08bc9a4 (feat: docker compose, db, prettier, ts config set up)

b752bbeb212e62987a041c9a304e6e32132b9922
spacylion <fuerstein@yandex.ru>
2024-12-14 23:51:14 +0300

    feat(auth): custom decorators and guards for auth and profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { RegisterDto } from '@/auth/dto/register.dto';
 import { AuthService } from './auth.service';
 import { LoggerService } from '@/logger/logger.service';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
 @Controller('auth')
 export class AuthController {
     constructor(
@@ -14,5 +15,24 @@ export class AuthController {
     @HttpCode(HttpStatus.CREATED)
     async register(@Req() req:Request, @Body() dto: RegisterDto) {
         return this.authService.register(req, dto);
+    }
+
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login(@Req() req:Request, @Body() dto: LoginDto) {
+        return this.authService.login(req, dto);
+    }
+
+
+    @Post('logout')
+    @HttpCode(HttpStatus.OK)
+    async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Promise<void> {
+        try {
+            await this.authService.logout(req, res);
+            res.status(HttpStatus.OK).send({ message: 'Logout successful' });
+        } catch (error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: error.message });
+        }
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthController } from '@/auth/auth.controller'
 import { AuthService } from '@/auth/auth.service'
 import { UserService } from '@/user/user.service'
 import { LoggerService } from '@/logger/logger.service'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService, UserService, LoggerService],
+	providers: [AuthService, UserService, LoggerService, ConfigService],
 	exports: [AuthService],
   })
   export class AuthModule {}
